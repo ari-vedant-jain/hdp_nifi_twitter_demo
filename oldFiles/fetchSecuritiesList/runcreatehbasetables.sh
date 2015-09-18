bb07d8e67d5d02488cc0f975fbabf899988e2738
vedantja <vedjain85@gmail.com>
2015-09-17 19:50:25 -0500

    Removing old files

--- oldFiles/fetchSecuritiesList/runcreatehbasetables.sh
+++
@@ -1,3 +0,0 @@
-echo "Creating Hbase table with thresholds"
-/usr/hdp/current/phoenix-client/bin/psql.py localhost:2181:/hbase-unsecure /root/oldFiles/fetchSecuritiesList/runcreatehbasetables.sh/fetchSecuritiesList/hbase-createstockthresholds.sql /root/oldFiles/fetchSecuritiesList/runcreatehbasetables.sh/fetchSecuritiesList/securities.csv
-
